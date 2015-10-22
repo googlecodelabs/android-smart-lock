7b1bfb5af24ecc128f894753c004669c520c39a5
Paul Matthews <pmatthews@google.com>
2015-10-22 14:23:55 -0500

    Step 10: Disable Auto Sign In On Sign Out
    * Add GoogleApiClient to ContentActivity
    * Implementing the GoogleApiClient connection callbacks
    * Set the logoutButton to disableAutoSignIn before navigating to the
    MainActivity

--- app/src/main/java/com/google/codelab/smartlock/ContentActivity.java
+++ app/src/main/java/com/google/codelab/smartlock/ContentActivity.java
@@ -16,10 +16,18 @@ package com.google.codelab.smartlock;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class ContentActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class ContentActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
 
     private static final String TAG = "ContentActivity";
 
@@ -28,14 +36,22 @@ public class ContentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, 0, this)
+                .addApi(Auth.CREDENTIALS_API)
+                .build();
+
         Button logoutButton = (Button) findViewById(R.id.logoutButton);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Auth.CredentialsApi.disableAutoSignIn(mGoogleApiClient);
                 Intent intent = new Intent(v.getContext(), MainActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
                 finish();
+
             }
         });
 
@@ -46,5 +62,20 @@ public class ContentActivity extends AppCompatActivity {
                 startActivity(new Intent(v.getContext(), ChangeCredActivity.class));
             }
         });
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "GoogleApiClient connected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.d(TAG, "GoogleApiClient is suspended with cause code: " + cause);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "GoogleApiClient failed to connect: " + connectionResult);
     }
 }
